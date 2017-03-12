be8acf8eb101283a4749f8bd05b7ac783818813a
Andy Tsai <andy@Andy-Tsai.local>
2017-03-12 18:08:38 +0800

    4. AddTodo - add changeText

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -4,14 +4,27 @@ import { Form, FormControl, Button } from 'react-bootstrap';
 export default class AddTodo extends Component {
     constructor() {
         super();
+        this.state = {
+            text: ''
+        };
+    }
+
+    changeText = (e) => {
+        this.setState({
+            text: e.target.value,
+        });
     }
 
     render() {
+        const { text } = this.state;
+
         return (
             <Form inline>
                  <FormControl
                     type="text"
                     placeholder="Todo..."
+                    onChange={this.changeText}
+                    value={text}
                 />
                 {' '}
                 <Button bsStyle="primary">Add</Button>
