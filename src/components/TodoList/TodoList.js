a7934a551fda268fab097e41cd11eca29c0c149f
Andy Tsai <andy@Andy-Tsai.local>
2017-03-12 17:54:06 +0800

    3. add TodoList content

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Table, Button } from 'react-bootstrap';
 
 export default class TodoList extends Component {
     constructor() {
@@ -7,9 +8,28 @@ export default class TodoList extends Component {
 
     render() {
         return (
-            <div>
-                TodoList
-            </div>
+            <Table bordered>
+                <thead>
+                    <tr>
+                        <th className="text-center" style={{width: '100px'}}>No.</th>
+                        <th className="text-center">Todo</th>
+                        <th className="text-center" style={{width: '100px'}}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="text-center">
+                            1
+                        </td>
+                        <td>
+                            Test
+                        </td>
+                        <td className="text-center">
+                            <Button bsStyle="danger">X</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         );
     }
 }
