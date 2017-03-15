bf4181eb78f851c6d1ba3f6dcfb47aba2568cf28
Andy Tsai <andy@Andy-Tsai.local>
2017-03-15 17:44:31 +0800

    9. add Provider

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import App from './containers/App';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 );
